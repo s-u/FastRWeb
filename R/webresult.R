dc0f9e3d9b69fe402a5bc73f85c3926b6332eec8
Simon Urbanek <simon.urbanek@r-project.org>
2011-04-26 17:37:52 +0000

    add empty default to payload
    git-svn-id: svn://svn.rforge.net/FastRWeb/trunk@9 75578478-82a9-43cf-90b0-15eb227bd808

--- R/webresult.R
+++ R/webresult.R
@@ -5,5 +5,5 @@ as.WebResult.WebResult <- function(x, ...) x
 as.WebResult.default <- function(x, ...)
   structure(c("html", paste(as.character(x),collapse='\n'), "text/html; charset=utf-8"), class="WebResult")
 
-WebResult <- function(cmd="html", payload, content.type="text/html; charset=utf-8", headers="")
-  structure(c(cmd, paste(as.character(payload), collapse="\n"), content.type, headers), class="WebResult")
+WebResult <- function(cmd="html", payload="", content.type="text/html; charset=utf-8", headers="")
+  structure(c(cmd, paste(as.character(payload), collapse="\n"), content.type, paste(as.character(headers),collapse="\r\n"), class="WebResult")
