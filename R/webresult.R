9420915112fe7265efa63f9e34396e1aef159519
Simon Urbanek <simon.urbanek@r-project.org>
2011-04-26 18:18:46 +0000

    fix typo
    git-svn-id: svn://svn.rforge.net/FastRWeb/trunk@16 75578478-82a9-43cf-90b0-15eb227bd808

--- R/webresult.R
+++ R/webresult.R
@@ -5,5 +5,5 @@ as.WebResult.WebResult <- function(x, ...) x
 as.WebResult.default <- function(x, ...)
   WebResult("html", paste(as.character(x), collapse='\n'))
 
-WebResult <- function(cmd="html", payload="", content.type="text/html; charset=utf-8", headers=character(0)) {
+WebResult <- function(cmd="html", payload="", content.type="text/html; charset=utf-8", headers=character(0))
   structure(c(cmd, paste(as.character(payload), collapse="\n"), content.type, paste(c(.headers, as.character(headers)),collapse="\r\n")), class="WebResult")
