400dac5766c3a5bc0f775b8dc178ce7f80875ad9
Simon Urbanek <simon.urbanek@r-project.org>
2012-02-27 16:03:19 +0000

    get the evaluation right
    git-svn-id: svn://svn.rforge.net/FastRWeb/trunk@40 75578478-82a9-43cf-90b0-15eb227bd808

--- R/run.R
+++ R/run.R
@@ -1,17 +1,16 @@
 .run <- function(request, root, path) {
   as.character(try({
-    webapi <<- 1.1
-    .out <<- ''
-    cmd <<- 'html'
-    ct <<- 'text/html'
-    hdr <<- ''
+    webapi <- 1.1
+    .e$.out <- ''
+    cmd <- 'html'
+    ct <- 'text/html'
     setwd(file.path(root, "tmp"))
 
     # deprecated compatibility settings - should go away...
-    requestURI <<- request$uri
-    remote.addr <<- request$client.ip
-    raw.cookies <<- request$raw.cookies
-    qs <<- request$query.string
+    requestURI <- request$uri
+    remote.addr <- request$client.ip
+    raw.cookies <- request$raw.cookies
+    qs <- request$query.string
 
     # parse parameters in the order of precedence: query string, multipart, urlencoded
     pars <- list()
@@ -24,23 +23,20 @@
     lapply(strsplit(strsplit(qs,'&')[[1]], '='),function(x) if (length(x) > 1L) pars[[x[1]]] <<- x[2])
   
     # find the script
-    request$path.info <<- ''
+    request$path.info <- ''
     sfn <- sprintf("%s/web.R/%s.R", root, path)
     if (!file.exists(sfn)) { # if the file doesn't exist, we try to separate script name from PATH_INFO
       left <- path
       while (nzchar(left <- gsub("/[^/]+$", "", left)) && !file.exists(cand <- sprintf("%s/web.R/%s.R", root, left))) {}
       if (!nzchar(left))
         return(c("html", paste("Script ", path, ".R not found", sep=''), "text/html", "Status: 404 Script not found"))
-      request$path.info <<- gsub(left, "", path, fixed=TRUE)
+      request$path.info <- gsub(left, "", path, fixed=TRUE)
       sfn <- cand
     }
 
-    ## again for compatibility with 0.x
-    .GlobalEnv$pars <- pars
-    
     if(exists('init') && is.function(init)) init()
 
-    source(sfn)
+    source(sfn, local=TRUE)
     as.WebResult(do.call(run, pars))
   }, silent=TRUE))
 }
