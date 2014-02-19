1a2c6f24c5dcc58596e3c765711192976ef39641
Simon Urbanek <simon.urbanek@r-project.org>
2014-02-19 17:26:06 -0500

    make sure jQuery gets loaded first

--- R/shiny.R
+++ R/shiny.R
@@ -1,2 +1,8 @@
+# thiny only has append, it doesn't have prepend so we have to create it
+tagPrependChild <- function(tag, child) {
+  tag$children <- c(list(child), tag$children)
+  tag
+}
+
 ## fix up shiny page ...
-auto.submit <- function(x) { x<-tagAppendChild(x, tags$script('$("#form").change(function() { $("#form").submit() }); var f=document.forms[0]; for (var i=0;f.elements[i]!=null;i++) f.elements[i].name=f.elements[i].id;')); x[[1]]<-tagAppendChild(x[[1]], tags$script(src="/shared/jquery.js")); x }
+auto.submit <- function(x) { x<-tagAppendChild(x, tags$script('$("#form").change(function() { $("#form").submit() }); var f=document.forms[0]; for (var i=0;f.elements[i]!=null;i++) f.elements[i].name=f.elements[i].id;')); x[[1]]<-tagPrependChild(x[[1]], tags$script(src="/shared/jquery.js")); x }
